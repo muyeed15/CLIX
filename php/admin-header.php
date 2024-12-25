d2d4f7142279c8d3fff031876f3d031138fe1212
muyeed15 <muyeed.al.abdullah@gmail.com>
2024-12-25 18:14:53 +0600

    Adjust margin-top for mobile view and improve code formatting in admin header

--- php/admin-header.php
+++ php/admin-header.php
@@ -64,6 +64,7 @@ function isActiveLink($pageName): string
     global $currentPage;
     return $currentPage === $pageName ? 'link-secondary' : 'link-body-emphasis';
 }
+
 ?>
 
 <header class="border-bottom" id="header-section">
@@ -77,50 +78,71 @@ function isActiveLink($pageName): string
             <!-- Navbar -->
             <nav class="d-none d-lg-flex flex-grow-1 justify-content-center">
                 <ul class="nav">
-                    <li><a href="./admin-dashboard.php" class="nav-link px-3 <?php echo isActiveLink('admin-dashboard.php'); ?>">Dashboard</a></li>
-                    <li><a href="./admin-outage.php" class="nav-link px-3 <?php echo isActiveLink('admin-outage.php'); ?>">Outage</a></li>
-                    <li><a href="./admin-IoT-control.php" class="nav-link px-3 <?php echo isActiveLink('admin-IoT-control.php'); ?>">IoT</a></li>
-                    <li><a href="./admin-notification.php" class="nav-link px-3 <?php echo isActiveLink('admin-notification.php'); ?>">Notification</a></li>
-                    <li><a href="./admin-login-seasion.php" class="nav-link px-3 <?php echo isActiveLink('admin-login-seasion.php'); ?>">Seasion</a></li>
-                    <li><a href="./admin-user-control.php" class="nav-link px-3 <?php echo isActiveLink('admin-user-control.php'); ?>">Client</a></li>
-                    <li><a href="./admin-feedback.php" class="nav-link px-3 <?php echo isActiveLink('admin-feedback.php'); ?>">Feedback</a></li>
+                    <li><a href="./admin-dashboard.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-dashboard.php'); ?>">Dashboard</a></li>
+                    <li><a href="./admin-outage.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-outage.php'); ?>">Outage</a></li>
+                    <li><a href="./admin-IoT-control.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-IoT-control.php'); ?>">IoT</a></li>
+                    <li><a href="./admin-notification.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-notification.php'); ?>">Notification</a>
+                    </li>
+                    <li><a href="./admin-login-seasion.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-login-seasion.php'); ?>">Seasion</a></li>
+                    <li><a href="./admin-user-control.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-user-control.php'); ?>">Client</a></li>
+                    <li><a href="./admin-feedback.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-feedback.php'); ?>">Feedback</a></li>
                 </ul>
             </nav>
 
             <!-- Notification, Mobile Navbar and User Section -->
             <div class="d-flex align-items-center">
                 <!-- Mobile Navbar Toggle -->
-                <button class="navbar-toggler d-lg-none" type="button" style="width: 50px; height: 50px;" data-bs-toggle="collapse" data-bs-target="#mobileNav" aria-controls="mobileNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon d-flex align-items-center justify-content-center" style="width: 100%; height: 100%;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#000000" class="bi bi-list" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                <button class="navbar-toggler d-lg-none" type="button" style="width: 50px; height: 50px;"
+                        data-bs-toggle="collapse" data-bs-target="#mobileNav" aria-controls="mobileNav"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon d-flex align-items-center justify-content-center"
+                              style="width: 100%; height: 100%;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#FFFFFF"
+                                 class="bi bi-list" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                      d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                             </svg>
                         </span>
                 </button>
 
                 <!-- Notifications -->
                 <div class="dropdown text-end me-2" id="notification-icon">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17px" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
+                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17px" fill="currentColor" class="bi bi-bell"
+                             viewBox="0 0 16 16">
+                            <path
+                                d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
                         </svg>
                     </a>
                     <ul class="dropdown-menu">
                         <?php while ($row = mysqli_fetch_assoc($notifications)) : ?>
-                            <li><a class="dropdown-item small" href="#"><?= htmlspecialchars($row['_notification_message_']); ?></a></li>
+                            <li><a class="dropdown-item small"
+                                   href="#"><?= htmlspecialchars($row['_notification_message_']); ?></a></li>
                         <?php endwhile; ?>
                     </ul>
                 </div>
 
                 <!-- User Picture -->
                 <div class="dropdown text-end" id="user-picture">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown">
-                        <img src="<?php echo $imageSrc; ?>" alt="User" class="rounded-circle" style="width: 36px; height: 36px;">
+                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                       data-bs-toggle="dropdown">
+                        <img src="<?php echo $imageSrc; ?>" alt="User" class="rounded-circle"
+                             style="width: 36px; height: 36px;">
                     </a>
                     <ul class="dropdown-menu text-small">
                         <li><a class="dropdown-item small" href="./admin-profile.php">Profile</a></li>
                         <li><a class="dropdown-item small" href="./admin-settings.php">Settings</a></li>
-                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
                         <li><a class="dropdown-item small" href="./logout.php">Sign out</a></li>
                     </ul>
                 </div>
@@ -131,13 +153,21 @@ function isActiveLink($pageName): string
         <div class="collapse" id="mobileNav">
             <nav class="navbar-nav">
                 <ul class="nav flex-column text-center">
-                    <li><a href="./admin-dashboard.php" class="nav-link px-3 <?php echo isActiveLink('admin-dashboard.php'); ?>">Dashboard</a></li>
-                    <li><a href="./admin-outage.php" class="nav-link px-3 <?php echo isActiveLink('admin-outage.php'); ?>">Outage</a></li>
-                    <li><a href="./admin-IoT-control.php" class="nav-link px-3 <?php echo isActiveLink('admin-IoT-control.php'); ?>">IoT</a></li>
-                    <li><a href="./admin-notification.php" class="nav-link px-3 <?php echo isActiveLink('admin-notification.php'); ?>">Notification</a></li>
-                    <li><a href="./admin-login-seasion.php" class="nav-link px-3 <?php echo isActiveLink('admin-login-seasion.php'); ?>">Seasion</a></li>
-                    <li><a href="./admin-user-control.php" class="nav-link px-3 <?php echo isActiveLink('admin-user-control.php'); ?>">Client</a></li>
-                    <li><a href="./admin-feedback.php" class="nav-link px-3 <?php echo isActiveLink('admin-feedback.php'); ?>">Feedback</a></li>
+                    <li><a href="./admin-dashboard.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-dashboard.php'); ?>">Dashboard</a></li>
+                    <li><a href="./admin-outage.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-outage.php'); ?>">Outage</a></li>
+                    <li><a href="./admin-IoT-control.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-IoT-control.php'); ?>">IoT</a></li>
+                    <li><a href="./admin-notification.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-notification.php'); ?>">Notification</a>
+                    </li>
+                    <li><a href="./admin-login-seasion.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-login-seasion.php'); ?>">Seasion</a></li>
+                    <li><a href="./admin-user-control.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-user-control.php'); ?>">Client</a></li>
+                    <li><a href="./admin-feedback.php"
+                           class="nav-link px-3 <?php echo isActiveLink('admin-feedback.php'); ?>">Feedback</a></li>
                 </ul>
             </nav>
         </div>
